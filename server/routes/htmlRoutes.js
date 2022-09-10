e9401464f852caae569d59f32c82ca5ffaf7691f
unknown <pratttrevor79@gmail.com>
2022-09-10 14:16:10 -0500

    merging develop and feature/client-server

---
+++ server/routes/htmlRoutes.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        res.sendFile(path.json(__dirname, '../../client/index.html'));
+    });
+};
